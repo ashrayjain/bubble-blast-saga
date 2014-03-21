2acc12fb3264c1fa2f971bca925caf8a0454ccae
Ashray Jain <ashray.jain@nus.edu.sg>
2014-03-21 15:59:32 +0800

    minor bug fix

--- ps05/ps05/ProjectileLaunchPath.m
+++ ps05/ps05/ProjectileLaunchPath.m
@@ -11,6 +11,7 @@
 #define DASH_LENGTH     10
 #define PATTERN_LENGTH  2
 #define DASH_WIDTH      5
+#define EDGE_THRESHOLD  30
 
 @interface ProjectileLaunchPath ()
 
@@ -37,7 +38,17 @@
     }
     UIBezierPath *aPath = [UIBezierPath bezierPath];
     [aPath moveToPoint:self.startPoint];
+    
+    self.endPoint = [self getExtendedEndPoint];
     [aPath addLineToPoint:self.endPoint];
+    CGPoint reflectedPoint = [self getReflectedPointForStartPoint:self.startPoint andEndPoint:self.endPoint];
+    [aPath addLineToPoint:reflectedPoint];
+    while (reflectedPoint.y > 0) {
+       CGPoint newReflectedPoint = [self getReflectedPointForStartPoint:self.endPoint andEndPoint:reflectedPoint];
+       self.endPoint = reflectedPoint;
+       reflectedPoint = newReflectedPoint;
+       [aPath addLineToPoint:reflectedPoint];
+    }
     CGFloat dashPattern[] = {DASH_LENGTH, DASH_LENGTH};
     [aPath setLineDash:dashPattern count:PATTERN_LENGTH phase:0];
     aPath.lineWidth = DASH_WIDTH;
@@ -61,6 +72,30 @@
 {
     _endPoint = endPoint;
     [self setNeedsDisplay];
+}
+
+- (CGPoint)getReflectedPointForStartPoint:(CGPoint)startPoint andEndPoint:(CGPoint)endPoint
+{
+    CGFloat x = (endPoint.x == 0)?self.frame.size.width:0;
+    CGFloat y = ((startPoint.y - endPoint.y) / (startPoint.x - endPoint.x)) * (x - startPoint.x) + startPoint.y;
+    CGFloat extrapolatedY = -y + 2 * endPoint.y;
+    return CGPointMake(x, extrapolatedY);
+}
+
+- (CGPoint)getExtendedEndPoint
+{
+    CGFloat xOffset = self.startPoint.x - self.endPoint.x;
+    if (fabs(xOffset) < 2.0) {
+        return CGPointMake(self.startPoint.x, 0);
+    }
+
+    CGFloat slope = (self.startPoint.y - self.endPoint.y) / xOffset;
+    CGFloat y = slope * (-self.startPoint.x) + self.startPoint.y;
+    if (y > self.endPoint.y) {
+        y = slope * (self.frame.size.width-self.startPoint.x) + self.startPoint.y;
+        return CGPointMake(self.frame.size.width, y);
+    }
+    return CGPointMake(0, y);
 }
 
 @end
